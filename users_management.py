9affaef9d3c02a9cb498c5a352abe1691ec0d3d7
Lara2005-bit <lsteiger@hcpa.edu.br>
2026-05-26 10:51:01 -0300

    Update users_management.py

--- users_management.py
+++ users_management.py
@@ -10,6 +10,7 @@ from botocore.exceptions import ClientError
 
 class UsersManagement:
     def __init__(self):
+        if 'AWS_ACCESS_KEY_ID' in st.secrets:
         os.environ['AWS_ACCESS_KEY_ID'] = st.secrets['AWS_ACCESS_KEY_ID']
         os.environ['AWS_SECRET_ACCESS_KEY'] = st.secrets['AWS_SECRET_ACCESS_KEY']
         os.environ['AWS_DEFAULT_REGION'] = st.secrets['AWS_DEFAULT_REGION']
