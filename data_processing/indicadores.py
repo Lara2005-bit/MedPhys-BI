eee23befbd3e7ffe1ade9eb9f97f116f5c9d1062
Lara2005-bit <lsteiger@hcpa.edu.br>
2026-06-23 09:30:48 -0300

    Update indicadores.py

--- data_processing/indicadores.py
+++ data_processing/indicadores.py
@@ -193,8 +193,6 @@ def calculate_indicadores(df_tests_need_to_do: pd.DataFrame) -> tuple:
         'not_done == False and `Sem material` == False'
     ).shape[0]
 
-    total_to_do = total - total_done
-
     indicador_realizacao = total_done / total * 100
 
     total_done_and_archived = df_tests_need_to_do.query(
@@ -206,5 +204,5 @@ def calculate_indicadores(df_tests_need_to_do: pd.DataFrame) -> tuple:
     return (
         indicador_realizacao,
         indicador_arquivado,
-        total_to_do
+        total
     )
