4477289158941dc3fcec1b78493154e363b3a013
Lara2005-bit <lsteiger@hcpa.edu.br>
2026-07-16 10:34:32 -0300

    Update tests_periodicity.py

--- tests_periodicity.py
+++ tests_periodicity.py
@@ -2,72 +2,133 @@
 
 class TestsPeriodicity():
     def __init__(self) -> None:
-        self.list_tests_gc_periodicity = self._set_gc_tests()
+        self.list_tests_gcventri_periodicity = self._set_gcventri_tests()
+        self.list_tests_gcinfinia_periodicity = self._set_gcinfinia_tests()
+        self.list_tests_gcdiscovery_periodicity = self._set_gcdiscovery_tests()
         self.list_tests_pet_periodicity = self._set_pet_tests()
         self.list_tests_curiometro_periodicity = self._set_curiometro_tests()
         self.list_tests_gm_periodicity = self._set_gm_tests()
         self.list_tests_gp_periodicity = self._set_gp_tests()
         
-    def _set_gc_tests(self):
-        list_tests_gc_periodicity = {
-            'Uniformidade intrínseca para alta densidade de contagem': 'Mensal',
-            'Resolução e linearidade espacial intrínseca': 'Mensal',
-            'Centro de rotação LEHR': 'Mensal',
-            'Centro de rotação MEGP': 'Mensal',
-            'Centro de rotação HEGP': 'Mensal',
-            'Resolução energética Tc-99m': 'Semestral',
-            'Resolução energética Tl-201': 'Semestral',
-            'Resolução energética Ga-67': 'Semestral',
-            'Resolução energética I-131': 'Semestral',
-            'Taxa máxima de contagem': 'Semestral',
-            'Resolução espacial íntriseca para fontes multi-energética I-131': 'Semestral',
-            'Resolução espacial íntriseca para fontes multi-energética Ga-67': 'Semestral',
-            'Resolução espacial íntriseca para fontes multi-energética Tl-201': 'Semestral',
-            'Corregistro espacial para fontes multi-energéticas Ga-67': 'Semestral',
-            'Corregistro espacial para fontes multi-energéticas Tl-201': 'Semestral',
-            'Sensibilidade planar Tc-99m': 'Semestral',
-            'Sensibilidade planar Ga-67': 'Semestral',
-            'Sensibilidade planar I-131': 'Semestral',
-            'Sensibilidade planar Tl-201': 'Semestral',
-            'Uniformidade extrínseca para alta densidade de contagem LEHR': 'Semestral',
-            'Uniformidade extrínseca para alta densidade de contagem MEGP': 'Semestral',
-            'Uniformidade extrínseca para alta densidade de contagem HEGP': 'Semestral',
-            'Verificação da angulação dos furos LEHR': 'Semestral',
-            'Verificação da angulação dos furos MEGP': 'Semestral',
-            'Verificação da angulação dos furos HEGP': 'Semestral',
-            'Velocidade da mesa em varreduras de corpo inteiro': 'Semestral',
-            'Desempenho geral da câmara SPECT': 'Semestral',
-            'Uniformidade íntrinseca para I-131': 'Anual',
-            'Uniformidade íntrinseca para Ga-67': 'Anual',
-            'Uniformidade íntrinseca para Tl-201': 'Anual',
-            'Uniformidade intrínseca com janelas energéticas assimétricas': 'Anual',
-            'Resolução e linearidade espacial extrínseca LEHR- Qualitativo': 'Anual',
-            'Resolução e linearidade espacial extrínseca MEGP- Qualitativo': 'Anual',
-            'Resolução e linearidade espacial extrínseca HEGP- Qualitativo': 'Anual',
-            'Resolução e linearidade espacial extrínseca LEHR- Quantitativo': 'Anual',
-            'Resolução e linearidade espacial extrínseca MEGP- Quantitativo': 'Anual',
-            'Resolução e linearidade espacial extrínseca HEGP- Quantitativo': 'Anual',
-            'Teste de angulação dos furos (LEHR)': 'Semestral', 
-            'Teste de angulação dos furos (HEGP)': 'Semestral', 
-            'Teste de angulação dos furos (MEGP)': 'Semestral',
-            'Uniformidade com alta densidade de contagens (VENTRI)': 'Mensal'
+    def _set_gcventri_tests(self):
+        list_tests_gcventri_periodicity = {
+            'Resolução e linearidade espacial intrínsecas': 'Mensal',
+            'Centro de rotção (COR)': 'Mensal',
+            'Uniformidade com alta densidade de contagens': 'Mensal',
+            'Resolução energética (Tc-99m)': 'Semestral',
+            'Resolução energética (Tl-201)': 'Semestral',
+            'Taxa de máxima contagem': 'Semestral',
+            'Resolução íntrinseca para fontes multi-energéticas(Tl-201)': 'Semestral',
+            'Co-registro espacial para fontes multi-energéticas (Tl-201)': 'Semestral',
+            'Teste de angulação dos furos': 'Semestral',
+            'Sensibilidade (Tc-99m)': 'Semestral',
+            'Sensibilidade (Ga-67)': 'Semestral',
+            'Uniformidade extrínseca': 'Semestral',
+            'Desempenho geral SPECT': 'Semestral',
+            'Uniformidade para radionuclídeos diferentes de TC-99m (Tl-201)': 'Anual',
+            'Janelas energéticas assimétricas': 'Anual',
+            'Resolução e linearidade espacial extrínsecas- Qualitativo': 'Anual',
+            'Resolução e linearidade espacial extrínsecas- Quantitativo': 'Anual'
         }
-        return list_tests_gc_periodicity
+        return list_tests_gcventri_periodicity
+
+    def _set_gcinfinia_tests(self):
+        list_tests_gcinfinia_periodicity = {
+            'Resolução e linearidade espacial intrínsecas': 'Mensal',
+            'COR (LEHR)': 'Mensal',
+            'COR (HEGP)': 'Mensal',
+            'COR (MEGP)': 'Mensal',
+            'Uniformidade com alta densidade de contagens': 'Mensal',
+            'Resolução energética (Tc-99m)': 'Semestral',
+            'Resolução energética (Tl-201)': 'Semestral',
+            'Resolução energética (Ga-67)': 'Semestral',
+            'Resolução energética (I-131)': 'Semestral',
+            'Taxa máxima de contagem': 'Semestral',
+            'Resolução instrínseca para fontes multi-energéticas (I-131)': 'Semestral',
+            'Resolução intrínseca para fontes multi-energéticas (Ga-67)': 'Semestral',
+            'Resolução intrínseca para fontes multi-energéticas (Tl-201)': 'Semestral',
+            'Co-registro espacial para fontes multi-energéticas (Ga-67)': 'Semestral',
+            'Co-registro espacial para fontes multienergéticas (Tl-201)': 'Semestral',
+            'Teste de angulação dos furos (LEHR)': 'Semestral',
+            'Teste de angulação dos furos (HEGP)': 'Semestral',
+            'Teste de angulação dos furos (MEGP)': 'Semestral',
+            'Sensibilidade (Tc-99m)': 'Semestral',
+            'Sensibilidade (Ga-67)': 'Semestral',
+            'Sensibilidade (I-131)': 'Semestral',
+            'Uniformidade extrínseca (LEHR)': 'Semestral',
+            'Uniformidade extrínseca (HEGP)': 'Semestral',
+            'Uniformidade extrínseca (MEGP)': 'Semestral',
+            'Velocidade da mesa': 'Semestral',
+            'Desempenho geral SPECT': 'Semestral',
+            'Uniformidade para radionuclídeos diferentes de Tc-99m (I-131)': 'Anual',
+            'Uniformidade para radionuclídeos diferentes de Tc-99m (Ga-67)': 'Anual',
+            'Uniformidade para radionuclídeos diferentes de Tc-99m (Tl-201)': 'Anual',
+            'Janelas energéticas assimétricas': 'Anual',
+            'Resolução e linearidade espacial extrínsecas (LEHR)- Qualitativo': 'Anual',
+            'Resolução e linearidade espacial extrínsecas (HEGP)- Qualitativo': 'Anual',
+            'Resolução e linearidade espacial extrínseca (MEGP)- Qualitativo': 'Anual',
+            'Resoução e linearidade espacial extrínseca (LEHR)- Quantitativo': 'Anual',
+            'Resolução e linearidade espacial extrínseca (HEGP)- Quantitativo': 'Anual',
+            'Resolução e linearidade espacial extrínseca (MEGP)- Quantitativo': 'Anual'
+        }
+        return list_tests_gcinfinia_periodicity
+
+    def _set_gcventri_tests(self):
+        list_tests_gcdiscovery_periodicity = {
+            'Resolução e linearidade espacial intrínsecas': 'Mensal',
+            'COR (LEHR)': 'Mensal',
+            'COR (HEGP)': 'Mensal',
+            'COR (MEGP)': 'Mensal',
+            'Uniformidade com alta densidade de contagens': 'Mensal',
+            'Resolução energética (Tc-99m)': 'Semestral',
+            'Resolução energética (Tl-201)': 'Semestral',
+            'Resolução energética (Ga-67)': 'Semestral',
+            'Resolução energética (I-131)': 'Semestral',
+            'Taxa máxima de contagem': 'Semestral',
+            'Resolução instrínseca para fontes multi-energéticas (I-131)': 'Semestral',
+            'Resolução intrínseca para fontes multi-energéticas (Ga-67)': 'Semestral',
+            'Resolução intrínseca para fontes multi-energéticas (Tl-201)': 'Semestral',
+            'Co-registro espacial para fontes multi-energéticas (Ga-67)': 'Semestral',
+            'Co-registro espacial para fontes multienergéticas (Tl-201)': 'Semestral',
+            'Teste de angulação dos furos (LEHR)': 'Semestral',
+            'Teste de angulação dos furos (HEGP)': 'Semestral',
+            'Teste de angulação dos furos (MEGP)': 'Semestral',
+            'Sensibilidade (Tc-99m)': 'Semestral',
+            'Sensibilidade (Ga-67)': 'Semestral',
+            'Sensibilidade (I-131)': 'Semestral',
+            'Uniformidade extrínseca (LEHR)': 'Semestral',
+            'Uniformidade extrínseca (HEGP)': 'Semestral',
+            'Uniformidade extrínseca (MEGP)': 'Semestral',
+            'Velocidade da mesa': 'Semestral',
+            'Desempenho geral SPECT': 'Semestral',
+            'Uniformidade para radionuclídeos diferentes de Tc-99m (I-131)': 'Anual',
+            'Uniformidade para radionuclídeos diferentes de Tc-99m (Ga-67)': 'Anual',
+            'Uniformidade para radionuclídeos diferentes de Tc-99m (Tl-201)': 'Anual',
+            'Janelas energéticas assimétricas': 'Anual',
+            'Resolução e linearidade espacial extrínsecas (LEHR)- Qualitativo': 'Anual',
+            'Resolução e linearidade espacial extrínsecas (HEGP)- Qualitativo': 'Anual',
+            'Resolução e linearidade espacial extrínseca (MEGP)- Qualitativo': 'Anual',
+            'Resoução e linearidade espacial extrínseca (LEHR)- Quantitativo': 'Anual',
+            'Resolução e linearidade espacial extrínseca (HEGP)- Quantitativo': 'Anual',
+            'Resolução e linearidade espacial extrínseca (MEGP)- Quantitativo': 'Anual'
+        }
+        return list_tests_gcdiscovery_periodicity
     
     def _set_pet_tests(self):
         list_tests_pet_periodicity = {
-            'Uniformidade e verificação da calibração do sistema PET-CT': 'Mensal',
-            'Alinhamento entre sistemas de laser PET/CT':'Sem periodicidade definida',
-            'Normalização e calibração cruzada': 'Trimestral',
-            'Resolução espacial (transversal e axial)': 'Semestral',
+            'Atualização do ganho': 'Semanal',
+            'Uniformidade/Verificação da calibração do sistema': 'Mensal',
+            'Atualização da correção da sincronização de coincidência (CTC)': 'Trimestral',
+            'Calibração cruzada e Normalização (PET 3D WCC)': 'Trimestral',
+            'Estabelecimento  de nova linha de base do PET DQA': 'Trimestral',
             'Sensibilidade': 'Trimestral',
-            'Corregistro das imagens de PET e CT': 'Semestral',
-            'Desempenho da taxa de contagens (NECR), taxa de eventos aleatórios, espalhados e verdadeiros, fração de espalhamento e exatidão das correções de eventos aleatórios e de perda de contagens': 'Anual',
-            'Desempenho geral e exatidão das correções de atenuação e espalhamento': 'Anual',
-            'Atualização dos ganhos': 'Semanal',
-            'Atualização da Correção da Sincronização de Coincidência (CTC)': 'Trimestral',
-            'Estabelecimento de nova linha de base do PET DQA': 'Trimestral',
-            'Fração de espelhamento': 'Anual'
+            'Corregistro PET-CT': 'Trimestral',
+            'Resolução espacial (tranversal e axial)': 'Semestral',
+            'Fração de espalhamento': 'Anual',
+            'Desempenho da taxa de contagem (NECR)': 'Anual',
+            'Taxa de eventos verdadeiros, aleatórios e espalhados': 'Anual',
+            'Acurácia: Exatidão nas correções de perda de contagens e eventos aleatórios': 'Anual',
+            'Desempenho geral do sistema e Exatidão das correções de espalhamento e atenuação': 'Anual'
         }
         return list_tests_pet_periodicity
     
@@ -94,15 +155,23 @@ class TestsPeriodicity():
         return list_tests_gp_periodicity
 
     def full_list(self):
-        return {**self.list_tests_gc_periodicity,
+        return {**self.list_tests_gcventri_periodicity,
+                **self.list_tests_gcinfinia_periodicity,
+                **self.list_tests_gcdiscovery_periodicity,
                 **self.list_tests_pet_periodicity,
                 **self.list_tests_curiometro_periodicity,
                 **self.list_tests_gm_periodicity,
                 **self.list_tests_gp_periodicity}
 
-    def map_gc_periodicity(self, name):
-        return self.list_tests_gc_periodicity[name]
+    def map_gcventri_periodicity(self, name):
+        return self.list_tests_gcventri_periodicity[name]
 
+    def map_gcinfinia_periodicity(self, name):
+        return self.list_tests_gcinfinia_periodicity[name]
+        
+    def map_gcdiscovery_periodicity(self, name):
+        return self.list_tests_gcdiscovery_periodicity[name]
+        
     def map_pet_periodicity(self, name):
         return self.list_tests_pet_periodicity[name]
 
